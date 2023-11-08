fa2351583467896aecb44866bf2c7699dd13bd9b
Anusha Mamidi <136111526+mamidi98@users.noreply.github.com>
2023-11-07 20:05:31 -0600

    Update Organization.java
    Added code changes as per the test requirement!

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,13 +1,17 @@
 package com.aa.act.interview.org;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public abstract class Organization {
 
     private Position root;
+
+    private int identifier;
     
     public Organization() {
         root = createOrganization();
+        this.identifier=1;
     }
     
     protected abstract Position createOrganization();
@@ -20,8 +24,41 @@ public abstract class Organization {
      * @return the newly filled position or empty if no position has that title
      */
     public Optional<Position> hire(Name person, String title) {
-        //your code here
-        return Optional.empty();
+        boolean isPresent = checkPosition(root, title);
+        if(!isPresent){
+            return Optional.empty();
+        }
+        Position position = new Position(title);
+        position.setEmployee(Optional.of(new Employee(createID(), person)));
+        position.addDirectReport(getDirectReport(title,root));
+        return Optional.of(position);
+    }
+
+    private Position getDirectReport(String title, Position root) {
+        if(root.getTitle().equals(title)){
+            return root;
+        }
+        Collection<Position> subordinates = root.getDirectReports();
+        for(Position position: subordinates){
+            Position currPosition = getDirectReport(title, position);
+            if(currPosition!=null) return currPosition;
+        }
+        return null;
+    }
+
+    private int createID() {
+        return this.identifier++;
+    }
+
+    private boolean checkPosition(Position root, String title) {
+        if(root.getTitle().equals(title)) return true;
+        Collection<Position> subordinates = root.getDirectReports();
+        for(Position position: subordinates){
+            if(checkPosition(position,title)){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
